5a154a93c8f11df23dbeaf4bf3d319177ca608e4
1mmanuelKant <joselisboaa05@gmail.com>
2023-05-07 22:33:39 -0300

    feat: add sqlalchemy to use DB

--- db.py
+++ db.py
@@ -1,12 +1,4 @@
-stores = {
-    1:  {
-        "name": "My Store",
-    }
-}
-items = {
-    1: {
-        "name": "Chair",
-        "price": 15.99,
-        "store_id": ""
-    }
-}
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
